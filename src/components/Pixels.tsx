f2597942b7c66cfa11fe9d529fac7e6d9635f0bf
IMEDMRK <ouakourimed@gmail.com>
2026-06-26 21:10:39 +0100

    Update src/components/Pixels.tsx

--- src/components/Pixels.tsx
+++ src/components/Pixels.tsx
@@ -1,8 +1,22 @@
 import Script from "next/script";
 import { GoogleAnalytics } from '@next/third-parties/google';
 
-export default function Pixels({ settings }: { settings: any }) {
+export default function Pixels({ settings, page }: { settings: any, page?: any }) {
   if (!settings) return null;
+
+  // Extract and combine Meta Pixels
+  const rawMetaPixels = [
+    ...(settings.metaPixelId ? settings.metaPixelId.split(',') : []),
+    ...(page?.metaPixelIds ? page.metaPixelIds.split(',') : [])
+  ];
+  const metaPixels = Array.from(new Set(rawMetaPixels.map(p => p.trim()).filter(Boolean)));
+
+  // Extract and combine TikTok Pixels
+  const rawTiktokPixels = [
+    ...(settings.tiktokPixelId ? settings.tiktokPixelId.split(',') : []),
+    ...(page?.tiktokPixelIds ? page.tiktokPixelIds.split(',') : [])
+  ];
+  const tiktokPixels = Array.from(new Set(rawTiktokPixels.map(p => p.trim()).filter(Boolean)));
 
   return (
     <>
@@ -12,7 +26,7 @@ export default function Pixels({ settings }: { settings: any }) {
       )}
 
       {/* Meta Pixel */}
-      {settings.metaPixelId && (
+      {metaPixels.length > 0 && (
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
@@ -23,19 +37,19 @@ export default function Pixels({ settings }: { settings: any }) {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${settings.metaPixelId}');
+            ${metaPixels.map(id => `fbq('init', '${id}');`).join('\n            ')}
             fbq('track', 'PageView');
           `}
         </Script>
       )}
 
       {/* TikTok Pixel */}
-      {settings.tiktokPixelId && (
+      {tiktokPixels.length > 0 && (
         <Script id="tiktok-pixel" strategy="afterInteractive">
           {`
             !function (w, d, t) {
               w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
-              ttq.load('${settings.tiktokPixelId}');
+              ${tiktokPixels.map(id => `ttq.load('${id}');`).join('\n              ')}
               ttq.page();
             }(window, document, 'ttq');
           `}
